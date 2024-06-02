9f97ee07978bb9c8150b949b7dc501a53e1168e4
Bunnavoit <vath24689@gmail.com>
2024-06-02 17:10:54 +0700

    just commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,16 @@
+import { ethers } from "hardhat";
+
 async function main() {
-  const [deployer] = await ethers.getSigners();
-
-  console.log("Deploying contracts with the account:", deployer.address);
-  console.log("Account balance:", (await deployer.getBalance()).toString());
-
   const EthLocker = await ethers.getContractFactory("EthLocker");
+
   const ethLocker = await EthLocker.deploy();
 
-  console.log("EthLocker address:", ethLocker.address);
+  await ethLocker.deployed();
+
+  console.log("EthLocker deployed to:", ethLocker.address);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
