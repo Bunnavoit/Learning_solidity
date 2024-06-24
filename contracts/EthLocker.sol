490ec3c7d14c47dbf5ba36cde25a03a9a6a40b1b
Bunnavoit <vath24689@gmail.com>
2024-06-24 08:32:58 +0700

    feat/add-strategy-vesting

--- contracts/EthLocker.sol
+++ contracts/EthLocker.sol
@@ -5,15 +5,19 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract EthLocker {
     address public owner;
-    uint256 public immutable lockDuration; 
+    uint256 public immutable lockDuration;
+    bool public isLinearVesting;
+
     mapping(address => uint256) public ethDeposits;
     mapping(address => uint256) public depositTimes;
 
     mapping(address => mapping(address => uint256)) public tokenDeposits;
+    mapping(address => uint256) public vestedAmounts;
 
     constructor(uint256 _initialLockDuration) {
         owner = msg.sender;
         lockDuration = _initialLockDuration;
+        isLinearVesting = false;
     }
 
     modifier onlyOwner() {
@@ -26,10 +30,15 @@ contract EthLocker {
         _;
     }
 
+    function setVestingStrategy(bool _isLinearVesting) external onlyOwner {
+        isLinearVesting = _isLinearVesting;
+    }
+
     function deposit() public payable {
         require(msg.value > 0, "Deposit amount must be greater than 0");
         ethDeposits[msg.sender] += msg.value;
         depositTimes[msg.sender] = block.timestamp;
+        vestedAmounts[msg.sender] = msg.value;
     }
 
     function depositTokens(address token, uint256 amount) public {
@@ -37,9 +46,26 @@ contract EthLocker {
         IERC20(token).transferFrom(msg.sender, address(this), amount);
         tokenDeposits[token][msg.sender] += amount;
         depositTimes[msg.sender] = block.timestamp;
+        vestedAmounts[msg.sender] = amount;
     }
 
-    function withdraw() external lockTimePassed {
+    function withdraw() external {
+        if (isLinearVesting) {
+            _withdrawLinearVesting();
+        } else {
+            _withdrawImmediateVesting();
+        }
+    }
+
+    function withdrawTokens(address token) external {
+        if (isLinearVesting) {
+            _withdrawTokensLinearVesting(token);
+        } else {
+            _withdrawTokensImmediateVesting(token);
+        }
+    }
+
+    function _withdrawImmediateVesting() internal lockTimePassed {
         uint256 amount = ethDeposits[msg.sender];
         require(amount > 0, "No funds to withdraw");
 
@@ -50,7 +76,7 @@ contract EthLocker {
         require(success, "Withdrawal failed");
     }
 
-    function withdrawTokens(address token) external lockTimePassed {
+    function _withdrawTokensImmediateVesting(address token) internal lockTimePassed {
         uint256 amount = tokenDeposits[token][msg.sender];
         require(amount > 0, "No funds to withdraw");
 
@@ -58,6 +84,37 @@ contract EthLocker {
         depositTimes[msg.sender] = 0;
 
         IERC20(token).transfer(msg.sender, amount);
+    }
+
+    function _withdrawLinearVesting() internal {
+        uint256 totalAmount = vestedAmounts[msg.sender];
+        uint256 vestingStartTime = depositTimes[msg.sender];
+        uint256 vestingDuration = block.timestamp - vestingStartTime;
+
+        uint256 vestedAmount = (totalAmount * vestingDuration) / lockDuration;
+        uint256 amountToWithdraw = vestedAmount - ethDeposits[msg.sender];
+
+        require(amountToWithdraw > 0, "No funds to withdraw");
+
+        ethDeposits[msg.sender] = vestedAmount;
+
+        (bool success, ) = msg.sender.call{value: amountToWithdraw}("");
+        require(success, "Withdrawal failed");
+    }
+
+    function _withdrawTokensLinearVesting(address token) internal {
+        uint256 totalAmount = vestedAmounts[msg.sender];
+        uint256 vestingStartTime = depositTimes[msg.sender];
+        uint256 vestingDuration = block.timestamp - vestingStartTime;
+
+        uint256 vestedAmount = (totalAmount * vestingDuration) / lockDuration;
+        uint256 amountToWithdraw = vestedAmount - tokenDeposits[token][msg.sender];
+
+        require(amountToWithdraw > 0, "No funds to withdraw");
+
+        tokenDeposits[token][msg.sender] = vestedAmount;
+
+        IERC20(token).transfer(msg.sender, amountToWithdraw);
     }
 
     function getRemainingLockTime(address user) external view returns (uint256) {
